906d90236648e379d0f833154ca6029657ef5d8a
Marco Napetti <marco.napetti@athonet.com>
2019-05-12 14:09:09 +0200

    Rust futureeeeeeeeeeees

--- e.rs
+++ e.rs
@@ -1,11 +1,18 @@
 #![feature(main)]
-#[cfg(feature = "functional")]
+#[cfg(all(feature = "functional", not(feature = "future")))]
 #[main]
 fn e() {
     std::iter::repeat('e').for_each(|e| print!("{}", e));
 }
 
-#[cfg(not(feature = "functional"))]
+#[cfg(all(not(feature = "functional"), feature = "future"))]
+#[main]
+fn e() {
+    use tokio::prelude::stream::Stream;
+    tokio::run(tokio::prelude::stream::iter_ok::<_, ()>(std::iter::repeat('e')).for_each(|e| { print!("{}", e); Ok(()) }));
+}
+
+#[cfg(all(not(feature = "functional"), not(feature = "future")))]
 #[main]
 fn e() -> ! {
     loop {
